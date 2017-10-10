a72a6823db329bff27c91f34e26d1589f32b60de
caijinhai <911159490@qq.com>
2017-10-10 21:11:11 +0800

    add index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>H5</title>
+    <link rel="stylesheet" href="css/lib/jquery.fullpage.min.css">
+</head>
+<body>
+    <div id="dowebok">
+        <div class="section">
+            <img src="img/bg1.jpg">
+        </div>
+        <div class="section">
+            <img src="img/bg2.jpg">
+        </div>
+    </div>
+</body>
+<script type="js/app.js"></script>
+<script type="js/lib/jquery.fullpage.min.js"></script>
+</html>
